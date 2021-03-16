df4f8c96484f77d051496c7a3b93c89e7f707f97
Juan Galarce <j6alarce@gmail.com>
2021-03-16 20:17:09 -0300

    Se crea la card para mostrar la informacion de cada recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -5,6 +5,15 @@ const Recipe = ({ recipe }) => {
     <div className="col-md-4 mb-3">
       <div className="card">
         <h2 className="card-header">{recipe.strDrink}</h2>
+        <img
+          className="card-img-top"
+          src={recipe.strDrinkThumb}
+          alt={recipe.strDrink}
+        />
+
+        <div className="card-body">
+            <button type="button" className="btn btn-block btn-primary">Ver Receta</button>
+        </div>
       </div>
     </div>
   );
